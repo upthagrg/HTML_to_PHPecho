3e11affdf1513fe0a47c75af3888a8d0c068b174
upthagrg <guglue4@gmail.com>
2016-10-29 00:05:16 -0700

    Update HTML_to_PHPecho.c

--- HTML_to_PHPecho.c
+++ HTML_to_PHPecho.c
@@ -11,13 +11,15 @@ This is then sent to a file of the users choice.
 #include <stdio.h>
 #include <string.h> 
 #include <sys/stat.h>
+#include <sys/types.h>
+#include<fcntl.h>
 #include <stdlib.h>
 
 /****************************************************************
 **Description: Gets the input from the user and copies it to a char*. 
 ****************************************************************/
 void get_input(char* return_string){
-  char* input_buffer[128];
+  char input_buffer[128];
   scanf("%128s", input_buffer);
   strcpy(return_string, input_buffer);
 }
@@ -39,12 +41,12 @@ int main(){
     memset(input, '\0', 128);
     printf("Enter the file you would like to write to> ");
     get_input(input);
-    output_file = open(input, O_RDONLY);
+    output_file = open(input, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if(input_file < 0){
       printf("ERROR OPENING FILE\n");
-    }
+    )
   }
-  clode(input_file);
+  close(input_file);
   close(output_file);
   return 0;
 }
